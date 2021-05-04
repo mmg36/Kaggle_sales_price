8042139bc2b817136555121777d0a19d60aa2ccd
mmg36 <mehdi.goudarzi@oodlefinance.com>
2021-05-04 23:49:29 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import plotly.express as px
+import numpy as np
+
+
+def test_results():
+    print("Overview of the data below: \n")
+    print(sales_train_df.head())
+    print("Some descriptive statistics below: \n")
+    print(sales_train_df.describe())
+
+
+def analysis():
+    corr_1 = sales_train_df['item_cnt_day'].corr(sales_train_df['item_price'])
+    print(sales_train_df.corr())
+    df = sales_train_df[sales_train_df['item_price'] > 0]
+    df = df.sort_values(by=['shop_id'])
+    fig = px.scatter(df, x='item_price', y='date_block_num', facet_col='shop_id',
+                     facet_col_wrap=7, facet_row_spacing=0.125)
+    fig.show()
+
+
+if __name__ == '__main__':
+    sales_train_df = pd.read_csv("./raw-data/sales_train.csv")
+    analysis()
+    #test_results()
