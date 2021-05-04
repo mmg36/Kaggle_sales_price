b2389d2acb2a51726f80aa52933ffc91df9d0d1c
mmg36 <mehdi.goudarzi@oodlefinance.com>
2021-05-05 00:11:32 +0100

    Preliminary result of raw dataset

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ def test_results():
 def analysis():
     corr_1 = sales_train_df['item_cnt_day'].corr(sales_train_df['item_price'])
     print(sales_train_df.corr())
-    df = sales_train_df[sales_train_df['item_price'] > 0]
+    df = sales_train_df[(sales_train_df['item_price'] > 15000) & (sales_train_df['item_price'] < 35000)]
     df = df.sort_values(by=['shop_id'])
-    fig = px.scatter(df, x='item_price', y='date_block_num', facet_col='shop_id',
-                     facet_col_wrap=7, facet_row_spacing=0.125)
+    fig = px.scatter(df, x='item_price', y='date_block_num', facet_col='shop_id', color = 'item_id',
+                     facet_col_wrap=10, facet_row_spacing=0.01, facet_col_spacing=0.01)
+    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightPink')
+    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightPink')
     fig.show()
 
 
